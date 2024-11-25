aac04c658e9890649dadb89e1236a17cc23c3254
jeannettearias <jeannettearias@icloud.com>
2024-11-25 21:33:19 +0100

    fetch the image

--- my_first_web/src/components/projects/ProjectImage.jsx
+++ my_first_web/src/components/projects/ProjectImage.jsx
@@ -5,12 +5,21 @@ import '../../styles/App.scss'
 function ProjectImage({ project }) {
 
     return (
-        <div
-            className='projectImage'
-            style={{
-                backgroundImage: project.project_image ? `url(${project.project_image})` : 'none',
-            }}
-        ></div>
+        <div className="projectImage">
+            {project.project_image ? (
+                <img
+                    src={project.project_image}
+                    alt={project.project_name || 'Project'}
+                    style={{
+                        width: '300px', // Set width
+                        height: '200px', // Set height
+                        objectFit: 'cover', // Ensure image fits nicely
+                    }}
+                />
+            ) : (
+                <p>No Image Available</p>
+            )}
+        </div>
     )
 }
 
@@ -18,6 +27,7 @@ ProjectImage.propTypes = {
     project: PropTypes.shape({
         idproject: PropTypes.number.isRequired,
         project_image: PropTypes.string,
+        project_name: PropTypes.string,
     }).isRequired,
 };
 
