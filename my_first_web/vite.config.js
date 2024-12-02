0ecc7da24e124ffcf62e73e930974a7225d42f4c
jeannettearias <jeannettearias@icloud.com>
2024-12-02 20:44:28 +0100

    updating package json and vite file

--- my_first_web/vite.config.js
+++ my_first_web/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'docs', // Change output directory to "docs"
+    emptyOutDir: true, // Ensures the directory is cleared before building
+  },
+});
+
