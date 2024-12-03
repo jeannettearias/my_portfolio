752e97a07e17137f8acdbc45549a128bc66646a0
jeannettearias <jeannettearias@icloud.com>
2024-12-03 18:48:53 +0100

    fix github pages deployment paths

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Replace with your repo name
+  base: '/my_projects/', // Replace with your repo name
   build: {
     outDir: 'docs',
     emptyOutDir: true,
