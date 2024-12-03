745887fa279fabbb7fc15b173987708a9b85febd
jeannettearias <jeannettearias@icloud.com>
2024-12-03 22:28:05 +0100

    fix github pages pushling

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: './',  // Ensure assets are loaded from the root for GitHub Pages
+  build: {
+    outDir: 'docs',  // Change the output directory to 'docs' for GitHub Pages
+  },
   server: {
     open: "/",
     watch: {
-      usePolling: true
-    }
-  }
+      usePolling: true,
+    },
+  },
 });
