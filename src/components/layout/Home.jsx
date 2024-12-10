bf490bfd82eb536e2a3a3b860ab06aa9be1404a0
jeannettearias <jeannettearias@icloud.com>
2024-12-10 20:51:11 +0100

    home style changes

--- src/components/layout/Home.jsx
+++ src/components/layout/Home.jsx
@@ -11,23 +11,23 @@ function Home() {
 
     return (
         <section className='home__section'>
-            <div>
+            <div className='photo__layout'>
                 <img className='home__picture' src="/img/me.jpeg" alt="" />
             </div>
             <div className='home__box'>
                 <h1 className='home__title' >Hi, I am Jeannette! 👋</h1>
                 <div className='home__content'>
                     <p className="prh__text">
-                        A seasoned professional with over 16 years of experience in the technology industry, including 10+ years in agile environments. Skilled in both product and management roles, I specialize in IT project management, financial oversight, and third-party vendor coordination. My expertise is reinforced by certifications in PMBOK Project Management, SAFe® 4 PM/PO, and Scrum Master (PSM I).
+                        With 16+ years in tech and a decade in agile environments, I excel in IT project management, financial planning, and vendor coordination. Certified in PMBOK, SAFe® 4 PM/PO, and PSM I, I deliver results with strategic focus and hands-on expertise.
                     </p>
                 </div>
+                <nav className='btn__home__box'>
+                    <div className='home__btn'>
+                        <button className='linkedin__btn' href='https://www.linkedin.com/in/jeannettearias/'
+                            onClick={handleLinkedinClick}>Go to LinkedIn</button>
+                    </div>
+                </nav>
             </div>
-            <nav className='btn__home__box'>
-                <div className='home__btn'>
-                    <button className='linkedin__btn' href='https://www.linkedin.com/in/jeannettearias/'
-                        onClick={handleLinkedinClick}>Go to LinkedIn</button>
-                </div>
-            </nav>
         </section >
     );
 }
