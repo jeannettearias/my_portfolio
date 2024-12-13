ace8081c83a939322e534576e098c9e642ec668c
jeannettearias <jeannettearias@icloud.com>
2024-12-13 16:25:01 +0100

    header update

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 import '../../styles/layout/_header.scss'
-import { useEffect, useState } from 'react';
-import JeannetteLogo from '../../images/Jeannette_logo.svg';
+import JeannetteLogo from '../../images/jean_logo.svg';
 
 function Header() {
     const handleContactMeClick = (ev) => {
@@ -9,26 +8,11 @@ function Header() {
         window.location.href = 'mailto:example@example.com?subject=Contact&body=Hello,';
     };
 
-    const [isTransparent, setIsTransparent] = useState(true);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 50) { // Adjust the scroll value as needed
-                setIsTransparent(false);
-            } else {
-                setIsTransparent(true);
-            }
-        };
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
 
     return (
 
         <div className='header__section'>
-            <header className={`header ${isTransparent ? 'transparent' : ''}`}>
+            <header className='header'>
                 <div className='logo__box'>
                     <img
                         className='logoSponsor'
@@ -36,28 +20,26 @@ function Header() {
                         alt="Jeannette's Logo"
                     />
                 </div>
-                <div>
-                    <nav className={`menu ${isTransparent ? 'transparent' : ''}`}>
-                        <ul className="menu__list ">
-                            <li className="menu__item">
-                                <Link to="/" className="menu__link">Home</Link>
-                            </li>
-                            <li className="menu__item">
-                                <Link to="/experience"
-                                    className="menu__link">Experience</Link>
-                            </li>
-                            <li className="menu__item">
-                                <Link to="/projects"
-                                    className="menu__link"> Projects </Link>
-                            </li>
-                            <li className="menu__item">
-                                <Link to="/contact"
-                                    className="menu__link">Contact</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-                <div>
+                <nav className='menu__box'>
+                    <ul className="menu__list ">
+                        <li className="menu__item">
+                            <Link to="/" className="menu__link">Home</Link>
+                        </li>
+                        <li className="menu__item">
+                            <Link to="/experience"
+                                className="menu__link">Experience</Link>
+                        </li>
+                        <li className="menu__item">
+                            <Link to="/projects"
+                                className="menu__link"> Projects </Link>
+                        </li>
+                        <li className="menu__item">
+                            <Link to="/contact"
+                                className="menu__link">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div className='btn__box'>
                     <Link to='/contact' className='contact__btn'
                         onClick={handleContactMeClick}>Contact</Link>
                 </div>
